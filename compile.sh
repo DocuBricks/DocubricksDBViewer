152646f4e2054deebf6037bdd400f8e6693ec562
moosd <souramoo@gmx.com>
2015-11-17 20:26:16 +0000

    Quick theming system to further separate data, processing, and styling

--- compile.sh
+++ compile.sh
@@ -3,18 +3,29 @@
 if [ "$#" -ne 1 ]; then
     echo "Please tell me which documentation file to compile..."
 fi
+theme=$2
+
+if [ "$#" -ne 2 ]; then
+    theme=$(cat theme)
+fi
 
 # first concat src files into one file
 rm out.html 2> /dev/null
+
 touch out.html
 
 (
-    cat src/header.html
+    cat src/themes/$theme/header.html
 
-    for f in src/css/*; do
+    for f in src/themes/$theme/css/*; do
         echo "<style type=\"text/css\">"
         cat $f
         echo "</style>"
+    done
+    for f in src/themes/$theme/js/*; do
+        echo "<script type=\"text/javascript\">"
+        cat $f
+        echo "</script>"
     done
     for f in src/js/*; do
         echo "<script type=\"text/javascript\">"
@@ -22,7 +33,7 @@ touch out.html
         echo "</script>"
     done
 
-    cat src/main.html
+    cat src/themes/$theme/main.html
 ) > out.html
 
 # then add in actual documentation file
